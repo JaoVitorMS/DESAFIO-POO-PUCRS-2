e7f5d23df39c640930ab01efc0d135c386faaec5
João Vitor <joao.vitorms1509@gmail.com>
2024-04-13 18:35:49 -0300

    Implementação da interface

--- Caixa.java
+++ Caixa.java
@@ -3,33 +3,29 @@ também os métodos sacar, depositar e transferir. O método transferir
  (Conta destino, double valor) deve permitir a transferência de um valor entre duas contas.
  irei ter que alterar o saldo que esta na classe Conta.java*/
 
- public class Caixa extends Conta{
+ public class Caixa extends Conta implements OperacoesBancarias{
     
     private double saque;
     private double depositar;
 
-    public Caixa(Int numerodaconta, double saldo){
+    public Caixa(int numerodaconta, double saldo){
         super(numerodaconta, saldo);
         this.saque = saque;
         this.depositar = depositar;
     }
-    
-    //metodo sacar, ira retirar um valor do saldo
-    public void sacar(double saque){
-        if(saque <= saldo){
-            sacar = saque - saldo;
+
+     @Override
+     public void depositar(double deposito) {
+         setSaldo(getSaldo()+deposito);
+     }
+
+     @Override
+     public void sacar(double saque) {
+        if(saque <= getSaldo()){
+            setSaldo(getSaldo()-saque);
         }
-        else if(saque > saldo){
-            System.out.println("Saldo insuficiente!")
+        else if(saque > getSaldo()){
+            System.out.println("Saldo insuficiente!");
         }
-    }
-
-    //metodo depositar, ira adicionar um valor ao saldo
-    public void deposito(double depositar){
-        deposito = depositar + saldo;
-    }  
-
-   
-
-
+     }
  }
